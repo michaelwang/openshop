1044d348e72ed469e0829d35075993e3f21efecf
michael <michael.zhenhua.wang@gmail.com>
2014-10-29 07:57:48 +0800

    refine ui

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Australopithecus\MenuBundle\AustralopithecusMenuBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {

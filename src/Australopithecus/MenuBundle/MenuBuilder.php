618cc5f5d1f4071c8f88e2a788054e70304b2162
michael <michael.zhenhua.wang@gmail.com>
2014-10-30 08:33:47 +0800

    add menu bundle

---
+++ src/Australopithecus/MenuBundle/MenuBuilder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Australopithecus\MenuBundle;
+
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+class MenuBuilder
+{
+    private $factory;
+
+    /**
+     * @param FactoryInterface $factory
+     */
+    public function __construct(FactoryInterface $factory)
+    {
+        $this->factory = $factory;
+    }
+
+    public function createMainMenu(Request $request)
+    {
+        $menu = $this->factory->createItem('root');
+
+        $menu->addChild('Products', array('route' => ''));
+        return $menu;
+    }
+}
